c2da645c3f90770507ff9ff706c4f1c1b2ee6679
Eduardo Buceta <ed_bucet@hotmail.com>
2019-12-16 21:43:52 -0600

    Agregando Elementos para Página en WPA

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<!-- Codificación de caracteres-->
+	<meta charset="utf-8">
+	<!-- Titulo de la Pagina Web-->
+	<title>Big IT México S.A de C.V.</title>
+	<!-- Configuración del Viewport Responsive-->
+	<meta name="Viewport" content="width=device-with, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+
+	<!-- Icono de la web -->
+	<link rel="shorcut icon" type="image/phg" href="img/favicon.png" />
+	<!--Hoja de Estilo-->
+	<link rel="stylesheet" href="css/styles.css" />
+</head>
+<body>
+<header id="main-header">
+	<div class="container">
+		<div id="logo">
+			<h1>BIG IT MEXICO</h1>
+		</div>
+		<div id="menu">
+			<ul>
+				<li>
+					<a href="#">
+						Inicio
+					</a>	
+				</li>
+				<li>
+					<a href="#">
+						Servicios
+					</a>	
+				</li>
+				<li>
+					<a href="#">
+						Videos
+					</a>	
+				</li>
+				<li>
+					<a href="#">
+						Contacto
+					</a>	
+				</li>
+			</ul>
+		</div> <!-- end menu-->
+	</div> <!-- end Container-->
+</header>
+<div class="clearfix"></div>
+<div id="slider">
+	<div class="container">
+		<h2>
+			Technology and Consulting Services
+		</h2>
+	</div>
+</div><!-- end slider-->
+<div id="services">
+	<div class="container">
+		<h2 class="subheader">
+			Servicios
+		</h2>
+		<div class="service">
+			<img src="img/1.png">
+			<h3>Desarrollo Web</h3>
+			<p>
+				Ofrezco servicio de Desarrollo Web Moderno con las mejores tecnologias web.
+			</p>
+		</div><!-- end servicio 1-->
+		<div class="service">
+			<img src="img/2.png">
+			<h3>Diseño Web</h3>
+			<p>
+				Diseño de sitios y aplicaciones Web moderno, Limpio y atractivo para tus clientes.
+			</p>
+		</div><!-- end servicio 2-->
+		<div class="service">
+			<img src="img/3.png">
+			<h3>Mentoria y Consultoria</h3>
+			<p>
+				Puedo ser tu profesor para enseñarte desarrollo y programación web moderna.
+			</p>
+		</div><!-- end servicio 3-->
+
+	</div>
+</div><!-- end services-->
+
+<div class="clearfix"></div>
+	<div id="videos">
+		<div class="container">
+			<h2 class="subheader">
+				Videos
+			</h2>
+			<div class="video">
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/Q8_G3XhumfE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; download" allowfullscreen></iframe>
+				<p>
+				El ataque al rey enemigo es uno de los principales elementos de la partida. Si quieres ganar al ajedrez necesitas aprender a atacar. El método que propone Luisón te dará las claves para saber cuándo puede funcionar un ataque y qué detalles pueden ser importantes.
+				</p>
+			</div><!-- end video 1-->
+			<div class="video">
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/I4MTu9WWow0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				<p>
+				Clase de ajedrez en directo en la que el M.F. Luis Fernández Siles nos introduce en la más importante de las aperturas abiertas: la Apertura Española o Ruy Lopez. Se trata de una de las más antiguas que existen, pero sin embargo se continua jugando en nuestros días a todos los niveles.
+				</p>
+			</div><!-- end video 2-->
+			<div class="video">
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/89X7ArI2hgQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				<p>
+				Conocer los principales trucos, trampas y celadas en las apertruas de ajedrez es muy importante para no perder rápidamente una partida y, además, para tener la posibilidad de ganar rápido al ajedrez.
+				</p>
+			</div><!-- end video 3-->
+		</div>
+	</div><!-- end videos-->
+	<div class="clearfix"></div>
+	<footer id="footer">
+		<div class="container">
+			<h2 class="subheader">Contacto</h2>
+			<p>Enviame un mensaje por cualquiera de las siguientes redes sociales</p>
+			<ul id="networks">
+				<li>
+					<a href="https://twitter.com/edbucet">
+						<img src="img/twitter.png"/>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<img src="img/facebook.png"/>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<img src="img/instagram.png"/>
+					</a>
+				</li>
+			</ul>
+			<div class="clearfix"></div>
+		</div>
+	</footer>
+</body>
+</html>
